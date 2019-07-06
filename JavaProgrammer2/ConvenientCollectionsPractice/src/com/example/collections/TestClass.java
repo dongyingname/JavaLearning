bea91d237620e0198f5789c41a49f2e6cb33c57f
Ying Dong <dongyingname@yahoo.com>
2019-07-05 20:13:31 -0600

    Update TestClass.java

--- JavaProgrammer2/ConvenientCollectionsPractice/src/com/example/collections/TestClass.java
+++ JavaProgrammer2/ConvenientCollectionsPractice/src/com/example/collections/TestClass.java
@@ -5,20 +5,34 @@
  */
 package com.example.collections;
 
+import java.time.*;
 import java.util.*;
+import static java.util.Map.entry;
 
 /**
  *
  * @author yingd
  */
 public class TestClass {
+
     public static void main(String[] args) {
-        List<Integer> testList=List.of(1,2,4,8,16,32);
+        List<Integer> testList = List.of(1, 2, 4, 8, 16, 32);
 //        testList.add(64);
 //        testList.set(2, 0);
-        
-        testList=List.of(0);
-        testList.forEach(x->System.out.println(x+","));
+
+        testList = List.of(0);
+        testList.forEach(x -> System.out.println(x + ","));
         System.out.println(" ");
+
+        Map<String, MonthDay> testMap = Map.ofEntries(
+                entry("Bologna Day", MonthDay.of(Month.JANUARY, 16)),
+                entry("Opposite Day", MonthDay.of(Month.JANUARY, 13)),
+                entry("Panic Day", MonthDay.of(Month.FEBRUARY, 9)),
+                entry("Raymond Day", MonthDay.of(Month.MARCH, 20)),
+                entry("Knight Day", MonthDay.of(Month.OCTOBER, 1))
+        );
+
+        testMap.forEach((key, value) -> System.err.println(key + ":" + value + ", "));
+
     }
 }
