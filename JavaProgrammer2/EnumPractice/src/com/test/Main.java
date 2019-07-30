6bf89092dc3a7abe0f69aa2798b8104c50a92713
Ying Dong <dongyingname@yahoo.com>
2019-07-30 14:03:59 -0600

    Create a test case to practice enumeration

---
+++ JavaProgrammer2/EnumPractice/src/com/test/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.test;
+
+/**
+ *
+ * @author yingd
+ */
+public class Main {
+    public static void main(String[] args) {
+        for(enm c : enm.values()) System.out.print(c+" ");
+    }
+}
+
+enum enm {
+ HEART, CLUB, SPADE, DIAMOND;
+}
